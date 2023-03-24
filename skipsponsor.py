b574f5638b607e39310a045fb2cc4725b3235f0f
Arham Aziz <arham1329@gmail.com>
2023-03-24 17:42:00 +0100

    added keywords

--- skipsponsor.py
+++ skipsponsor.py
@@ -6,11 +6,11 @@ from pytube import YouTube
 # Parsing arguments from command line
 parser = argparse.ArgumentParser(description='Remove ads from video')
 parser.add_argument('source', help='Enter YouTube video id')
-parser.add_argument('dest_file', help='Enter destination output file')
+# parser.add_argument('dest_file', help='Enter destination output file')
 args = parser.parse_args()
 
 source_id = args.source
-dest_file = args.dest_file
+# dest_file = args.dest_file
 
 def is_sponsored(frame):
     """Detects if frame has a sponsored segment in it
@@ -26,7 +26,7 @@ def is_sponsored(frame):
 
     text = pytesseract.image_to_string(gray)
 
-    keywords = ["sponsored", "promoted", "free trial"]
+    keywords = ["sponsored", "promoted", "free trial", "revolut", "sponsor", "with code", "code:", "discovery+"]
     for keyword in keywords:
         if keyword in text.lower():
             print(keyword)
@@ -56,7 +56,7 @@ def cap_from_youtube(yt_id):
 
     return cap
 
-def skip_sponsored(yt_id, output_path):
+def skip_sponsored(yt_id):
     """Detects if frame has a sponsored segment in it
 
     Parameters:
@@ -73,8 +73,6 @@ def skip_sponsored(yt_id, output_path):
     frameRate = cap.get(cv2.CAP_PROP_FPS)
     allFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), frameRate, (frameWidth, frameHeight))
-
     sponsorStart = None
     sponsorEnd = None
 
@@ -90,13 +88,13 @@ def skip_sponsored(yt_id, output_path):
             print(f"Sponsored segment from {sponsorStart/frameRate:.2f}s to {sponsorEnd/frameRate:.2f}s")
     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    for i in range(allFrames):
-        ret, frame = cap.read()
-        if not ret:
-            break
-        if i < sponsorStart or i > sponsorEnd:
-            out.write(frame)
-    cap.release()
-    out.release()
+    # for i in range(allFrames):
+    #     ret, frame = cap.read()
+    #     if not ret:
+    #         break
+    #     if i < sponsorStart or i > sponsorEnd:
+    #         out.write(frame)
+    # cap.release()
+    # out.release()
 
-skip_sponsored(source_id, dest_file)
+skip_sponsored(source_id)
